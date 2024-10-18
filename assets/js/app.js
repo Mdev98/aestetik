fbf33421c6627bc8fbc034395186934030dabeb4
Mdev98 <mamour.dev98@gmail.com>
2024-10-18 17:56:32 +0000

    optimize element loading, rearragent css file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,25 @@
-function loadSVG () {
-    fetch("assets/images/logo-aestetik.svg")
-    .then((response) => { return response.text(); })
-    .then((svg) => {
-        document.querySelector(".logo").innerHTML = svg;
-    });
-    fetch("assets/images/aestetik.svg")
-    .then((response) => { return response.text(); })
-    .then((svg) => {
-        document.querySelector(".hero-banner").innerHTML = svg;
-    });
-    document.body.style.display = 'block';
+function fetchSVG(url, selector) {
+    return fetch(url)
+        .then(response => response.text())
+        .then(svg => {
+            document.querySelector(selector).innerHTML = svg;
+        });
 }
 
-loadSVG();
+function loadSVG() {
+    // Use requestAnimationFrame to ensure the browser is not blocked while loading the SVGs
+    requestAnimationFrame(() => {
+        Promise.all([
+            fetchSVG("assets/images/logo-aestetik.svg", ".logo"),
+            fetchSVG("assets/images/aestetik.svg", ".hero-banner")
+        ]).then(() => {
+            // Show the body after SVGs are loaded
+            document.body.style.display = 'block';
+        }).catch(error => {
+            console.error('Error loading SVG:', error);
+        });
+    });
+}
 
-
+// Lazy load SVGs after the window has loaded
+window.addEventListener('load', loadSVG);
