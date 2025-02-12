a03a9f13336ea852401de361cd90f5f2d9b643b2
Mdev98 <mamour.dev98@gmail.com>
2025-02-12 22:30:57 +0000

    service detail page : code to handle data added layout done

---
+++ assets/js/service-detail.js
@@ -0,0 +1,183 @@
+const serviceDetails = {
+    'design-identite-visuelle': {
+        title: "Design d'identité visuelle",
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        items: [
+            {
+                title: 'Service Item 1',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 2',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 3',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 4',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 5',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 6',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            }
+        ]
+    },
+    'design-print' : {
+        title: "Design print",
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        items: [
+            {
+                title: 'Service Item 1',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 2',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 3',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 4',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 5',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 6',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            }
+        ]
+    },
+    'design-web' : {
+        title: "Design web",
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        items: [
+            {
+                title: 'Service Item 1',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 2',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 3',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 4',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 5',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 6',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            }
+        ]
+    },
+    'strategie-contenu-digital' : {
+        title: "Stratégie de contenu digital",
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        items: [
+            {
+                title: 'Service Item 1',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 2',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 3',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 4',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 5',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 6',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            }
+        ]
+    },
+    'accompagnement-personnalise' : {
+        title: "Accompagnement personnalisé",
+        content: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+        items: [
+            {
+                title: 'Service Item 1',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 2',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 3',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 4',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 5',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            },
+            {
+                title: 'Service Item 6',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Donec eget'
+            }
+        ]
+    },
+}
+
+
+
+
+function loadServiceDetails() {
+    const url = window.location.href;
+    const decodedUrl = decodeURIComponent(url);
+    const serviceName = decodedUrl.split("#")[1];
+    
+    const service = serviceDetails[serviceName];
+    if (!service) {
+        window.location.href = "./service.html";
+        return;
+    }
+
+    document.querySelector(".service-content-title").textContent = service.title;
+    document.querySelector(".service-content-text").textContent = service.content;
+    
+    const serviceItems = document.querySelectorAll(".service-content-item");
+    serviceItems.forEach((item, index) => {
+        const title = item.querySelector(".service-content-item-title");
+        const text = item.querySelector(".service-content-item-text");
+        
+        if (service.items[index]) {
+            title.innerHTML = service.items[index].title;
+            text.textContent = service.items[index].content;
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", loadServiceDetails);
