ac143a298bab582f6646fb29740ce0c82019c9bd
Mamour DIOP <89989089+Mdev98@users.noreply.github.com>
2025-03-07 16:56:02 +0000

    Update app.js retrait du bloc bloquant le chargement de la page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,1 @@
-window.onload = function() {
-    document.body.style.visibility = "visible"; // Affiche la page une fois chargée
-};
-
-// Masquer la page au début
-document.body.style.visibility = "hidden";
+console.log("Hello dummy !");
