de936f09ade2ff88f82a6dc97a25560c3be1538b
Mdev98 <mamour.dev98@gmail.com>
2025-03-14 17:07:08 +0000

    Page contact created and done

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    document.body.style.visibility = "visible"; // Affiche la page une fois chargée
-};
-
-// Masquer la page au début
-document.body.style.visibility = "hidden";
